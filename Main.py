b0682d10431a27718afdb6a034d6ea958f245566
yousifgaming <96596211+yousifhub@users.noreply.github.com>
2024-04-06 15:58:08 +0200

    V1 inital upload

---
+++ Main.py
@@ -0,0 +1,52 @@
+import json
+import random
+from tkinter import *
+
+def stanger():
+    with open('C://Users//Asus//Desktop//coding//Python//NewPhoneWhoDis//stranger.json', 'r') as file:
+        data = json.load(file)
+    list = data["stranger"]
+    response = random.choice(list)
+    return response
+
+def you():
+    with open('C://Users//Asus//Desktop//coding//Python//NewPhoneWhoDis//you.json', 'r') as file:
+        data = json.load(file)
+    list = data["you"]
+    response = random.choice(list)
+    return response
+
+def close_and_reopen():
+    root.destroy()
+    app()
+   
+def app():
+    global root
+    
+    root = Tk()
+    root.title("NewPhoneWhoDis!")
+    icon_path = "C:\\Users\\Asus\\Desktop\\coding\\Python\\NewPhoneWhoDis\\icon.png"
+    icon = PhotoImage(file=icon_path)
+    root.iconphoto(False, icon)
+    
+    lbl = Label(root, text="Stranger: ")
+    lbl.grid()
+    
+    lbl = Label(root, text=stanger(), wraplength=300)
+    lbl.grid()
+    
+    lbl = Label(root, text="\n", wraplength=300)
+    lbl.grid()
+    
+    lbl = Label(root, text="You: ")
+    lbl.grid()
+    
+    lbl = Label(root, text=you(), wraplength=300)
+    lbl.grid()
+    
+    btn = Button(root, text="Reroll", fg="red", command=close_and_reopen)
+    btn.grid(column=2, row=0)
+    
+    root.mainloop()
+    
+app()
